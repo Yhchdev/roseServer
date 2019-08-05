dc608202b37b72172a5c462c94b3c0eeb7a7725d
Yhchdev <630673943@qq.com>
2019-08-05 18:10:55 +0800

    1.数据库迁移到本地;2.flask 整合mqtt主动发送消息

--- configs.py
+++ configs.py
@@ -1,10 +1,9 @@
 # 数据库配置
-
-
+HOSTNAME = '127.0.0.1'
+DATABASE = 'rose'
+USERNAME = 'root'
+PASSWORD = 'yhchdev666'
 
 DB_URI = 'mysql://{}:{}@{}/{}'.format(
     USERNAME, PASSWORD, HOSTNAME, DATABASE
 )
-
-
-#
--- run.py
+++ run.py
@@ -6,11 +6,9 @@ from flask_socketio import SocketIO
 from app.user import user
 from app.rose import rose
 
-
 app = Flask(__name__)
-CORS(app,  resources={r"/*": {"origins": "*"}})   # 允许所有域名跨域
+CORS(app, resources={r"/*": {"origins": "*"}})  # 允许所有域名跨域
 CORS(app, supports_credentials=True)
-
 
 app.config['SECRET'] = 'yhchdev'
 app.config['TEMPLATES_AUTO_RELOAD'] = True
@@ -23,20 +21,35 @@ app.config['MQTT_TLS_ENABLED'] = False
 mqtt = Mqtt(app)
 socketio = SocketIO(app)
 
-app.register_blueprint(user, url_prefix = '/user')
-app.register_blueprint(rose, url_prefix = '/rose')
+app.register_blueprint(user, url_prefix='/user')
+app.register_blueprint(rose, url_prefix='/rose')
 
 
 
+# 连接后 订阅主题
 @mqtt.on_connect()
 def handle_connect(client, userdata, flags, rc):
+    mqtt.subscribe('rose/pic_base64')
     print("Connected with result code " + str(rc))
 
 
 
+# 收到消息后 执行的方法
 @mqtt.on_message()
 def handle_mqtt_message(client, userdata, message):
     print(message.topic + " " + str(message.payload))
+
+
+
+
+# 发布主题和信息
+@app.route('/mqtt/pub/<want_to_pub>', methods=['GET'])
+def pub_my_msg(want_to_pub):
+    mqtt.publish('rose/control',want_to_pub)
+    return want_to_pub
+
+
+
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=5000, use_reloader=True, debug=True)
